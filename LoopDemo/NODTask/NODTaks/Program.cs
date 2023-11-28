0d787d6411899de18b82c1cd68cf007543de00cd
Ruslan <ruslan.hilinsky@yandex.ru>
2023-11-29 01:45:28 +0300

    Задание на алгоритм и сумму цифр в числе

---
+++ LoopDemo/NODTask/NODTaks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace modu_2_
+{
+    class LoopProgram
+    {
+        static void Main()
+        {
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //while (a != b)
+            //{
+            //    if (a > b)
+            //        a -= b;
+            //    else
+            //        b -= a;
+
+            //}
+            //int nod = a;
+            //Console.WriteLine("nod = " + nod);
+
+            Console.WriteLine("Please enter the number:");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            Console.WriteLine($"The sum of the digits of the number: {sum}");
+        }
+    }
+}
---
+++ MySqrt/MySqrt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqrt
+{
+    internal class Program
+    {
+        public static double CalculateSqrt(double target)
+        {
+            double x = 1;
+            double oldx;
+            do
+            {
+                oldx = x;
+                x = (x + target / x) / 2;
+            }
+            while (oldx != x);
+            return x;
+        }
+        static void Main()
+        {
+            double target = 2023;
+            double result = CalculateSqrt(target);
+            Console.WriteLine(result);
+            Console.WriteLine(result * result);
+            Console.ReadLine();
+        }
+    }
+}
